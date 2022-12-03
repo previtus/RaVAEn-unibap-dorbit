665bc67135fdb9b9b06db3b147e12d75b1963dbe
previtus <previtus@gmail.com>
2022-12-03 22:49:04 +0000

    batch of 2 is not needed

--- run_experimental.sh
+++ run_experimental.sh
@@ -3,12 +3,10 @@
 # Inference over different batch sizes:
 # python3.7 run_inference.py
 
-python3.7 ravaen_payload/run_inference.py --batch_size 2 --save_only_k_latents 1
 python3.7 ravaen_payload/run_inference.py --batch_size 4 --save_only_k_latents 1
 python3.7 ravaen_payload/run_inference.py --batch_size 8 --save_only_k_latents 1
 python3.7 ravaen_payload/run_inference.py --batch_size 16 --save_only_k_latents 1
 
-python3.7 ravaen_payload/run_inference.py --batch_size 2 --override_channels 10 --log_name "highres10band" --nosave True
 python3.7 ravaen_payload/run_inference.py --batch_size 4 --override_channels 10 --log_name "highres10band" --nosave True
 python3.7 ravaen_payload/run_inference.py --batch_size 8 --override_channels 10 --log_name "highres10band" --nosave True
 python3.7 ravaen_payload/run_inference.py --batch_size 16 --override_channels 10 --log_name "highres10band" --nosave True
